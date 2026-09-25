c2280a37e3e6786eedf17f4c3cd6111fb479b440
Tahera Akter <taherascript@gmail.com>
2026-09-25 16:31:46 +0600

    workouts detail page is done

--- src/app/my-plan/page.tsx
+++ src/app/my-plan/page.tsx
@@ -7,8 +7,12 @@ const MyPlan = () => {
     console.log(add,save,'list')
     return (
         <div className='container mx-auto py-10'>
-            <h2 className='font-bold text-[30px] font-oswald'>MY PLAN</h2>
-            <p className='text-[14px] font-normal font-inter text-[#8A92A0]'>Cap of five lifts for today. Finish them, then load more.</p>
+
+           <div> <h2 className='font-bold text-[30px] font-oswald'>MY PLAN</h2>
+            <p className='text-[14px] font-normal font-inter text-[#8A92A0]'>Cap of five lifts for today. Finish them, then load more.</p></div>
+            <div>
+                
+            </div>
         </div>
     );
 };
